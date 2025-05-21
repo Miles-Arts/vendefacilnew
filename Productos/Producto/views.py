b4671612bf8a3a6e93fcd2f9d3d2241b3c2a9fb8
Milton <gratno@gmail.com>
2025-05-21 15:51:21 -0500

    Actualización de plantilla gestionProducto.html y ajustes en INSTRUCTIONS.md

--- Productos/Producto/views.py
+++ Productos/Producto/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
 def home(request):
-    return render(request, "gestionProducto.html")
+    productos= Producto.objects.all()
+    return render(request, "gestionProducto.html", {"productos":productos})
