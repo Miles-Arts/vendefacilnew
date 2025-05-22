41fefcc1d6258755fa8fb85cd4edd2798126dda9
Milton <gratno@gmail.com>
2025-05-21 21:34:12 -0500

    Updated gestionProducto.html and other project files

--- Productos/Producto/views.py
+++ Productos/Producto/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import Producto
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     productos= Producto.objects.all()
+    messages.success(request, '¡Productos Listados!')
     return render(request, "gestionProducto.html", {"productos":productos})
 
 def registrarProducto(request):
@@ -14,7 +16,7 @@ def registrarProducto(request):
     peso=request.POST['numPeso']
     
     producto=Producto.objects.create(codigo=codigo,nombre=nombre,peso=peso)
-    
+    messages.success(request, '¡Productos Registrado!')
     return redirect('/')
 
 def edicionProducto(request,codigo):
@@ -31,10 +33,14 @@ def editarProducto(request):
     producto.peso=peso
     producto.save()
     
+    messages.success(request, '¡Producto Actualizado!')
+    
     return redirect('/')
 
 def eliminarProducto(request, codigo):
     producto=Producto.objects.get(codigo=codigo)
     producto.delete()
+    
+    messages.success(request, '¡Producto Eliminado!')
     
     return redirect('/')
