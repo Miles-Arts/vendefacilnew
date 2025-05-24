015dbc26a2ec06cece5ff31862e2fbbf6862d5bd
Milton <gratno@gmail.com>
2025-05-23 23:11:27 -0500

    Se actualizo en fondo y se esta creando la logica del login.

--- VendeFacil/urls.py
+++ VendeFacil/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Productos.Producto.urls'))
+    path('', include('Productos.Producto.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 # Servir archivos multimedia en desarrollo
