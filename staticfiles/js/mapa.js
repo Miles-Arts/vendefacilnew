3cbe4876eab1e5c9f20ec3da65286f87aabcb369
Milton <gratno@gmail.com>
2025-05-25 18:31:28 -0500

    Creación de favicon

---
+++ staticfiles/js/mapa.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+        var map = L.map('map').setView([5.690338844121654, -73.22773931589995], 9);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">Colombia - Boyacá</a> - Municipio Tuta'
+    }).addTo(map);
+
+    L.marker([5.690338844121654, -73.22773931589995 ]).addTo(map)
+        .bindPopup("<b>Tuta Boyacá</b><br> Oficina VendeFacil app ✅")
+        .openPopup();
+});
