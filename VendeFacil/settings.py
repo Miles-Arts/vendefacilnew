5b4cba5aeeac53648019fe7320c859fbaae8edc8
Milton <gratno@gmail.com>
2025-05-26 18:42:05 -0500

    Nuevo pip Cripsy Bootstrap5

--- VendeFacil/settings.py
+++ VendeFacil/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
